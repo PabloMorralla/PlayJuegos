896f5bf3204df50c187ed8e84e9a7bf0cc110eb2
Pablo Morralla <pabmordel2@alu.edu.gva.es>
2025-09-22 20:37:17 +0200

    Añadida página New Player (Ahora sí)

--- app/src/main/java/com/example/playjuegos/MainActivity.kt
+++ app/src/main/java/com/example/playjuegos/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,22 +13,34 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -71,9 +84,10 @@ fun HomeScreen(navController: NavHostController, modifier: Modifier = Modifier) 
             Spacer(modifier = Modifier.height(50.dp))
 
             val texts = arrayOf("Play", "New Player", "Preferences", "About")
-            for (i in texts) {
-                HomeButton(text = i)
-            }
+            HomeButton("Play")
+            HomeButton("New Player", navController = navController, link="NewPlayer")
+            HomeButton("Preferences")
+            HomeButton("About")
         }
     }
     else {
@@ -87,13 +101,13 @@ fun HomeScreen(navController: NavHostController, modifier: Modifier = Modifier) 
 
             val texts = arrayOf("Play", "New Player", "Preferences", "About")
             Row {
-                HomeButton(texts[0])
-                HomeButton(texts[1])
+                HomeButton("Play")
+                HomeButton("New Player")
             }
             Spacer(modifier.height(-10.dp))
             Row {
-                HomeButton(texts[2])
-                HomeButton(texts[3])
+                HomeButton("Preferences")
+                HomeButton("About")
             }
         }
     }
@@ -111,10 +125,12 @@ fun Title(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun HomeButton(text: String, modifier: Modifier = Modifier) {
+fun HomeButton(text: String, modifier: Modifier = Modifier, link: String? = null, navController: NavHostController? = null) {
     Button (
-        onClick = {},
-        modifier = modifier.width(300.dp).height(50.dp),
+        onClick = { navController?.navigate(link ?: "") },
+        modifier = modifier
+            .width(300.dp)
+            .height(50.dp),
         colors = ButtonDefaults.buttonColors(
             containerColor = MaterialTheme.colorScheme.primary
         )
@@ -123,12 +139,104 @@ fun HomeButton(text: String, modifier: Modifier = Modifier) {
             text = text
         )
     }
-    Spacer(Modifier.height(20.dp).width(20.dp))
+    Spacer(Modifier
+        .height(20.dp)
+        .width(20.dp))
 }
 
 @Composable
 fun NewPlayerScreen(modifier: Modifier = Modifier) {
-    Text(text = "New Player Screen")
+    var nameFieldState by remember { mutableStateOf(" ") }
+    var surnameFieldState by remember { mutableStateOf(" ") }
+    var nickFieldState by remember { mutableStateOf(" ") }
+    var phoneFieldState by remember { mutableStateOf(" ") }
+    var emailFieldState by remember { mutableStateOf(" ") }
+
+    Column (
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row {
+            Icon(
+                imageVector = Icons.Rounded.AccountCircle,
+                contentDescription = "Icono de Perfil",
+                tint = Color.DarkGray,
+                modifier = Modifier.size(60.dp)
+            )
+            TextField(
+                value = nameFieldState,
+                onValueChange = {nameFieldState = it},
+                label = { Text(text = "Nombre") }
+            )
+        }
+        Spacer(modifier = Modifier.size(25.dp))
+        Row {
+            Spacer(modifier = Modifier.size(60.dp))
+            TextField(
+                value = surnameFieldState,
+                onValueChange = {surnameFieldState = it},
+                label = { Text(text = "Apellido") }
+            )
+        }
+        Spacer(modifier = Modifier.size(25.dp))
+        Row {
+            Spacer(modifier = Modifier.size(60.dp))
+            TextField(
+                value = nickFieldState,
+                onValueChange = {nickFieldState = it},
+                label = { Text(text = "Apodo") }
+            )
+        }
+        Spacer(modifier = Modifier.size(25.dp))
+        Image(
+            painter = painterResource(id = R.drawable.android),
+            contentDescription = "Imágen de Android",
+            modifier = Modifier.requiredSize(150.dp)
+        )
+        Spacer(modifier = Modifier.size(25.dp))
+        Row {
+            Icon(
+                imageVector = Icons.Rounded.Phone,
+                contentDescription = "Icono de Teléfono",
+                tint = Color.DarkGray,
+                modifier = Modifier.size(60.dp)
+            )
+            TextField(
+                value = phoneFieldState,
+                onValueChange = {phoneFieldState = it},
+                label = { Text(text = "Teléfono") }
+            )
+        }
+        Spacer(modifier = Modifier.size(25.dp))
+        Row {
+            Icon(
+                imageVector = Icons.Rounded.Email,
+                contentDescription = "Icono de Correo",
+                tint = Color.DarkGray,
+                modifier = Modifier.size(60.dp)
+            )
+            TextField(
+                value = emailFieldState,
+                onValueChange = {emailFieldState = it},
+                label = { Text(text = "Correo Electrónico") }
+            )
+        }
+        Spacer(modifier = Modifier.size(50.dp))
+        Button(
+            onClick = {},
+            modifier = modifier
+                .width(300.dp)
+                .height(50.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primary
+            )
+        ) {
+            Text(
+                text = "Guardar Cambios"
+            )
+        }
+    }
 }
 
 // ----- PREVIEWS -----
