cb35ce07e05b20d0ff50644d8935941d1fde956f
Pablo Morralla <pabmordel2@alu.edu.gva.es>
2025-10-03 20:54:25 +0200

    3/10/2025

--- app/src/main/java/com/example/playjuegos/MainActivity.kt
+++ app/src/main/java/com/example/playjuegos/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.playjuegos
 
 import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,34 +13,43 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.rounded.AccountCircle
 import androidx.compose.material.icons.rounded.Email
 import androidx.compose.material.icons.rounded.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -252,21 +262,44 @@ fun PreferencesScreen(modifier: Modifier = Modifier, navController: NavHostContr
             modifier = modifier.fillMaxSize(),
         ) {
             Text(
-                text = "Elige una opción:"
+                modifier = modifier.fillMaxWidth(),
+                text = "Elige una opción:",
+                textAlign = TextAlign.Center
             )
 
             val texts: Array<String> = arrayOf("Angry Birds", "Dragon Fly", "Hill Climbing Racing", "Pocket Soccer", "Radiant Defense", "Ninja Jump", "Air Control")
+            var opcion by rememberSaveable { mutableStateOf("-") }
             for (text in texts) {
                 Row {
                     RadioButton(
-                        selected = false,
-                        onClick = {}
+                        selected = (opcion == text),
+                        onClick = {opcion = text}
                     )
                     Text (text = text)
                 }
             }
 
+            val range = 0f..10f
+            val steps = 8
+            var selection by remember {mutableStateOf(0f)}
+
+            Slider (
+                value = selection,
+                valueRange = range,
+                steps = steps,
+                onValueChange = {selection = it}
+            )
+
             HomeButton("Volver", navController = navController, link = "Home")
+        }
+
+        FloatingActionButton(
+            onClick = {
+                Toast.makeText(LocalContext.current, Toast.LENGTH_LONG).show()
+            },
+            modifier = modifier
+        ) {
+            Icon(Icons.Filled.CheckCircle, "Floating action button")
         }
     }
 }
